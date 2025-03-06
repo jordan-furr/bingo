135e7f43c0e68b1e3b6cb224dfd1f7e11c41f2d6
jordan-furr <jordanfurr98@gmail.com>
2025-03-06 09:24:11 +0100

    button games

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import CountButton from "./count-button";
+
+export default async function Page() {
+    const colors = ['orange', 'red', 'pink'];
+    return (
+        <main>
+            <div>
+                <h2>You've got this</h2>
+                <h3>My favorite colors:</h3>
+                <ul>
+                    {colors.map((color) => (
+                        <li key={color}>{color}</li>
+                    ))}
+                </ul>
+                <CountButton />
+            </div>
+        </main>
+    );
+} 
