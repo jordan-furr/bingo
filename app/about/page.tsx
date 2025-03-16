acbe311bc3e847a4e306a58d37460073eaa1a072
jordan-furr <jordanfurr98@gmail.com>
2025-03-16 13:30:33 +0100

    auth

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,7 @@
 import '@/app/globals.css';
 import styles from "@/app/page.module.css";
 
+
 export default function Page() {
     return (
         <div className={styles.page}>
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./page.module.css";
 
+
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -10,7 +11,7 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <Link
-          href="/about"
+          href="/login"
         >
           <Image
             aria-hidden
